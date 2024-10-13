14d40ed24915bbd7864b421422abf4dc7a88ed04
mdimransorker <apgimran@gmnail.com>
2024-10-13 21:55:39 +0600

    Credit Card Validation

---
+++ credit_card_validation.py
@@ -0,0 +1,10 @@
+#-----------------------Credit card validator in python-------------------------
+
+#1. Remove any '_' or ' ' 
+# 2. Add all digits in the odd places from right to left 
+# 3. Double every second digit from right to left. 
+#    ( If result is a two-digit number, 
+#     add the two-digit number together to get a single digit) 
+# 4. Sum the totals of step 2 & 3 
+# 5. If sum is divisible by 10, the credit card #is valid
+
