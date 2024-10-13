2e2188ff2998cfd017dd56cf7668d8da4d5a0e92
mdimransorker <apgimran@gmnail.com>
2024-10-13 22:47:18 +0600

    Data Input

--- credit_card_validation.py
+++ credit_card_validation.py
@@ -12,10 +12,33 @@ sum_odd_digits = 0
 sum_even_digits = 0
 total = 0
 
-#step 1
+#--------------------step 1----------------------
 
 card_number = input("Enter your credit card number #: ")
 card_number = card_number.replace("-","" )
 card_number = card_number.replace(" ", "")
 card_number = card_number[:: -1]
-print(card_number)
+# print(card_number)
+
+#--------------------------------Step 2------------------
+for x in card_number[:: 2]:
+    sum_odd_digits += int(x)
+
+#-------------------------------Step3--------------------
+
+for x in card_number[1 :: 2]:
+    x = int(x) *2
+    if x >= 10:
+        sum_even_digits += (1 + (x % 10))
+    else:
+        sum_even_digits += x
+
+#------------------------ step 4------------------
+total = sum_odd_digits + sum_even_digits
+
+
+#--------------------step 5-------------
+if total  % 10 == 0:
+   print("Valid")
+else:
+    print("Invalid")
