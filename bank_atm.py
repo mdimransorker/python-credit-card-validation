d90dd88aafd2ca4655b97224b8a551d81ff0aece
mdimransorker <apgimran@gmnail.com>
2024-10-15 07:00:32 +0600

    Bank atm simple projet complit

--- bank_atm.py
+++ bank_atm.py
@@ -1,14 +1,14 @@
-def show_balance():
+def show_balance(balance):
     print(f"This is your balance: ${balance: .2f}")
 def deposit_balance():
-    amount = float(input("Please Enter Your Deposit Amount"))
+    amount = float(input("Please Enter Your Deposit Amount: "))
     if amount < 0:
         print("Deposit amount can't 0")
         return 0
     else:
         return amount
-def withdraw_balance():
-    amount = float(input("Enter your Withdraw amount"))
+def withdraw_balance(balance):
+    amount = float(input("Enter your Withdraw amount: "))
     if amount > balance:
         print("Insufficient Balance")
         return 0
@@ -17,28 +17,34 @@ def withdraw_balance():
         return 0
     else:
         return amount
+def main():
+    balance =0 
+    is_running = True
 
-balance =0 
-is_running = True
+    while is_running:
+        print("This BRAC Bank ATM")
+        print("***************************************")
+        print("1. Show Your Balance")
+        print("2.Deposit Your Balance")
+        print("3. Withdraw Your Balance")
+        print("4.Exit")
+        print()
+        print("*******************************")
 
-while is_running:
-    print("This BRAC Bank ATM")
-    print("1. Show Your Balance")
-    print("2.Deposit Your Balance")
-    print("3. Withdraw Your Balance")
-    print("4.Exit")
+        choice = input("Please Enter your Choice: ")
 
-    choice = input("Please Enter your Choice")
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit_balance()
+        elif choice == '3':
+            balance -= withdraw_balance(balance)
+        elif choice == "4":
+            is_running = False
+        else:
+            print("This is not valid")
 
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        deposit_balance()
-    elif choice == '3':
-        withdraw_balance()
-    elif choice == "4":
-        is_running = False
-    else:
-        print("This is not valid")
+    print("Thanks for choose our ATM")
 
-print("Thanks for choose our ATM")
+if __name__ == "__main__":
+    main()
