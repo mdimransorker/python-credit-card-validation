4bcbd38f1800455e3da2eda376685c380a7c33ec
mdimransorker <apgimran@gmnail.com>
2024-10-15 06:39:39 +0600

    Basice Structure Done

---
+++ bank_atm.py
@@ -0,0 +1,18 @@
+def show_balance():
+    pass
+def deposit_balance():
+    pass
+def withdraw_balance():
+    pass
+
+balance =0 
+is_running = True
+
+while is_running:
+    print("This BRAC Bank ATM")
+    print("1. Show Your Balance")
+    print("2.Deposit Your Balance")
+    print("3. Withdraw Your Balance")
+    print("4.Exit")
+
+    choice = input("Please Enter your Choice")
